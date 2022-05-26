b85b3ef695e59cf27a5c6d2ff42d835a80bcf323
Daniel Kuzmenko <kuzmenko.dani@gmail.com>
2022-05-26 13:03:05 +0300

    Creating introduction, User input of type of degrees

---
+++ Temperature Calculator/TemperatureCalculator.py
@@ -0,0 +1,9 @@
+introduction = """Hello, this is a temperature calculator program that
+converts Celsius degrees to Fahrenheit and vice versa
+"""
+print(introduction)
+
+get_degree = input ("What type of degree you want to convert? (Celsius/Fahrenheit) ")
+get_degree = get_degree.lower()
+
+if
