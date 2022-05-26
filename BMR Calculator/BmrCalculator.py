ffbe82d438da37880175f2ceda82c1e8ee1be293
Daniel Kuzmenko <kuzmenko.dani@gmail.com>
2022-05-26 15:54:23 +0300

    Object that will determine the calculation in imperial/metric method.

---
+++ BMR Calculator/BmrCalculator.py
@@ -0,0 +1,4 @@
+introduction = """Hello, this is a program that calculates your BMR
+and also can calculate your BF% to help your weight loss journey.
+"""
+get_unit = input("Which unit you would like to work with? (imperial/metric)")
