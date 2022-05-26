3dffa32a627e4ae80962fcc0a43d0e31b6ba9303
Daniel Kuzmenko <kuzmenko.dani@gmail.com>
2022-05-26 15:58:51 +0300

    A function to calculate the BMR of the user in metric units

--- BMR Calculator/BmrCalculator.py
+++ BMR Calculator/BmrCalculator.py
@@ -1,4 +1,13 @@
 introduction = """Hello, this is a program that calculates your BMR
 and also can calculate your BF% to help your weight loss journey.
 """
+print(introduction)
+
 get_unit = input("Which unit you would like to work with? (imperial/metric)")
+get_unit = get_unit.lower()
+
+if get_unit == "metric":
+    user_age = input ("Enter your age here: ")
+    user_height = input("Enter your height in inches ")
+    user_weight = input ("Enter your weight in pounds ")
+    
