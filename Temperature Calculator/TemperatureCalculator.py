ed9d8559e6c19909e94c33111bbd71014dfaf155
Daniel Kuzmenko <kuzmenko.dani@gmail.com>
2022-05-26 13:12:42 +0300

    function of fahrenheit to celsius

--- Temperature Calculator/TemperatureCalculator.py
+++ Temperature Calculator/TemperatureCalculator.py
@@ -10,4 +10,8 @@ if get_degree =="celsius":
     celsius_degree = input("Enter the celsius degree: ")
     celsius_to_fahrenheit = (celsius_degree*1.8) + 32
     print(f"That is {celsius_to_fahrenheit} Fahrenheit")
-    
+
+elif get_degree == "fahrenheit":
+fahrenheit_degree = input("Enter the fahrenheit degree: ")
+fahrenheit_to_celsius = (fahrenheit_degree-32)/1.8
+print(f"that is {fahrenheit_to_celsius} celsius")
